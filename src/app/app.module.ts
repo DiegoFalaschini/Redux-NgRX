c592bcc8ace189c46ab1cf938043a71a668090af
Diego Falaschini <falaschini.diego@GrupoFava.local>
2020-03-13 15:02:46 -0300

    Hasta capítulo: 'Utils Ruter', inclusive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule,  routerReducer} from "@ngrx/router-store";
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+const NGRX_IMPORTS = [
+  StoreModule.forRoot( reducers, { metaReducers }),
+  StoreRouterConnectingModule.forRoot({ stateKey: 'router'}),
+  EffectsModule.forRoot( [] ),
+  StoreDevtoolsModule.instrument( {
+    name: 'AngularChatNgRx',
+    logOnly: environment.production
+  })
+]
+
+import { reducers, metaReducers } from "./reducers/reducers";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +26,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...NGRX_IMPORTS
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/interfaces/IUser';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  user: IUser;
+
+  constructor( private authService: AuthService ) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+
+    this.authService.login( this.user ).subscribe( 
+      (response) => {
+        console.log( response );
+        
+      }
+    )
+  }
+
+}
